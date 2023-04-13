0278af801f2763b66e7973d057b443cdec2808db
Christian Wygoda <christian.wygoda@wygoda.net>
2023-04-13 23:51:47 +0200

    Update template

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -2,7 +2,7 @@
   import Calendar from "$lib/components/organisms/Calendar.svelte";
 </script>
 
-<div class="mx-auto w-[65ch]">
+<div class="mx-auto max-w-[65ch]">
   <div class="prose mb-8">
     <h1 class="font-knewave font-normal text-sky-600">Running Calendar</h1>
     <p>Enjoy browsing our social running calendar for the next twelve months.</p>
