29e8055648cf80977beb5ccdf8f59952e117ee10
Christian Wygoda <christian.wygoda@wygoda.net>
2023-04-14 11:59:07 +0200

    Add calendar swiping

--- src/lib/components/molecules/CalendarHeader.svelte
+++ src/lib/components/molecules/CalendarHeader.svelte
@@ -4,29 +4,14 @@
   import type { Time } from "ical.js";
 
   export let currentMonth: Time;
-  export let months: Time[];
+  export let go: (offset: number) => void;
+  export let atStart: boolean;
+  export let atEnd: boolean;
 
-  $: atStart = months.at(0)?.compare(currentMonth) == 0;
-  $: atEnd = months.at(-1)?.compare(currentMonth) == 0;
   const format = new Intl.DateTimeFormat("en-US", {
     month: "long",
     year: "numeric",
   });
-
-  const go = (offset: number) => {
-    let targetMonth = currentMonth.clone();
-    targetMonth.month += offset;
-    const start = months.at(0);
-    const end = months.at(-1);
-    if (start && targetMonth.compare(start) == -1) {
-      targetMonth = start;
-    }
-    if (end && targetMonth.compare(end) == +1) {
-      targetMonth = end;
-    }
-
-    currentMonth = targetMonth;
-  };
 </script>
 
 <div class="flex items-center justify-center gap-4 fill-sky-600 text-sky-600">
