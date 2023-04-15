7e8fa405b2cd68bfe8752847f7ffffdb2165876a
Christian Wygoda <christian.wygoda@wygoda.net>
2023-04-15 12:49:02 +0200

    Fetch ical after page load, not before

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
+  import { browser } from "$app/environment";
+
+  import ICal from "$lib/calendar";
   import Calendar from "$lib/components/organisms/Calendar.svelte";
 
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
+  let ical: ICal | undefined;
+  onMount(async () => {
+    if (browser) {
+      const ical_response = await fetch("/calendar/running.ical");
+      ical = new ICal(await ical_response.text());
+    }
+  });
 </script>
 
 <div class="mx-auto max-w-[65ch]">
@@ -11,7 +20,7 @@
     <h1 class="font-knewave font-normal text-sky-600">Running Calendar</h1>
     <p>Enjoy browsing our social running calendar for the next twelve months.</p>
   </div>
-  <Calendar calendar={data.ical} />
+  <Calendar calendar={ical} />
   <div class="prose mt-4">
     <p>
       You can also subscribe this calendar in ICAL format, just follow this link: <a
